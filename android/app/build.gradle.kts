a6f985330e1579b5be12b3d488a23e3dd63b7724
grazzy <grazzy7124@gmail.com>
2025-08-25 23:10:11 +0900

    sdk 버전 오류 해결

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,11 @@ plugins {
 }
 
 android {
+
+    
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.whatapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -26,11 +28,12 @@ android {
         applicationId = "com.example.whatapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
 
     buildTypes {
         release {
